15e53cbd45af44404b94abe4e8a4689a6b006049
Ethan Rambacher <eerambach@gmail.com>
2017-12-03 16:58:03 -0500

    Added LED code

--- signalling_demo/signalling_demo.py
+++ signalling_demo/signalling_demo.py
@@ -2,12 +2,16 @@ from dumby_motion_sensor import MotionSensor
 from dumby_rssiparser import BluetoothRanger
 import os
 import random as rand
+import RPi.GPIO as GPIO
 
 class SmartDoor(object):
     def __init__(self):
         self.ms = MotionSensor(75, 40, 1000, 1000, 20, 20, False, self.motion_detected, 0.8)
         self.thresh_hold_x = 500
         self.br = BluetoothRanger({"FF:FF:80:00:86:56"},-80)
+        self.led_pin = 21
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.led_pin,GPIO.OUT)
 
     def motion_detected(self, x, y):
         print "Signalling Subsytem Recieved Camera Notification"
@@ -27,9 +31,9 @@ class SmartDoor(object):
 
     def alert(self):
         print "\n$$$ Sending Flag to Alert Subsystem $$$"
+        GPIO.output(self.led_pin, GPIO.HIGH)
         os.system("mplayer leave_room.m4a > stupid_data.txt")
-        #YO CAN YOU DO THE SET UP FOR THE LED?
-        # keep led on for 1 second
+        GPIO.output(self.led_pin, GPIO.LOW)
 
 
     def run(self):
